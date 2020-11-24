361c2cee69bf2ff888ec68e3c9afe6a854db0d6d
vassil-at <72628283+vassil-at@users.noreply.github.com>
2020-11-24 21:28:52 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,53 +1,47 @@
-public class BogoSort {
-    public static void main(String[] args) {
-        //Enter array to be sorted here
-        int[] arr = {4, 5, 6, 0, 7, 8, 9, 1, 2, 3};
-
-        BogoSort now = new BogoSort();
-        System.out.println("Unsorted: ");
-        now.display1D(arr);
-
-        now.bogo(arr);
-
-        System.out.println("Sorted:");
-        now.display1D(arr);
-    }
-
-    void bogo(int[] arr) {
-        //Keep a track of the number of shuffles
-        int shuffle = 1;
-        for (; !isSorted(arr); shuffle++)
-            shuffle(arr);
-        //Boast
-        System.out.println("This took " + shuffle + " shufles.");
-    }
-
-    void shuffle(int[] arr) {
-        //Standart Fisher-Yates shuffle algorithm
-        int i = arr.length - 1;
-        while (i > 0)
-            swap(arr, i--, (int) (Math.random() * i));
-    }
-
-    void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    boolean isSorted(int[] arr) {
-
-        for (int i = 1; i < arr.length; i++)
-            if (arr[i] < arr[i - 1])
-                return false;
-        return true;
-    }
-
-    void display1D(int[] arr) {
-        for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i] + " ");
-        System.out.println();
-    }
-}
+import java.util.Arrays;
+class cocktailSort
+{
+    void coctailSord(int nums[])
+    {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i <= nums.length - 2; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    //test if two elements are in the wrong order
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+                {
+                    swapped = false;
+                    for (int i = nums.length - 2; i >= 0; i--) {
+                        if (nums[i] > nums[i + 1]) {
+                            int temp = nums[i];
+                            nums[i] = nums[i + 1];
+                            nums[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                }
+                while (swapped) ;
+            }
+            // Method to test above
+            public static void main (String args[])
+            {
+                cocktailSort ob = new cocktailSort();
+                int nums[] = {7, 5, 3, 2, 1, 12, 45};
+                System.out.println("Original Array:");
+                System.out.println(Arrays.toString(nums));
+                ob.coctailSord(nums);
+                System.out.println("Sorted Array");
+                System.out.println(Arrays.toString(nums));
+            }
+        }
 
 
+                        
