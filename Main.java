a0064b0e86e998ad4b7481284319c203641ff590
vassil-at <72628283+vassil-at@users.noreply.github.com>
2020-11-25 13:45:16 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,47 +1,31 @@
 import java.util.Arrays;
-class cocktailSort
-{
-    void coctailSord(int nums[])
-    {
-        boolean swapped;
-        do {
-            swapped = false;
-            for (int i = 0; i <= nums.length - 2; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    //test if two elements are in the wrong order
-                    int temp = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = temp;
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
-                {
-                    swapped = false;
-                    for (int i = nums.length - 2; i >= 0; i--) {
-                        if (nums[i] > nums[i + 1]) {
-                            int temp = nums[i];
-                            nums[i] = nums[i + 1];
-                            nums[i + 1] = temp;
-                            swapped = true;
-                        }
-                    }
-                }
-                while (swapped) ;
-            }
-            // Method to test above
-            public static void main (String args[])
-            {
-                cocktailSort ob = new cocktailSort();
-                int nums[] = {7, 5, 3, 2, 1, 12, 45};
-                System.out.println("Original Array:");
-                System.out.println(Arrays.toString(nums));
-                ob.coctailSord(nums);
-                System.out.println("Sorted Array");
-                System.out.println(Arrays.toString(nums));
+class countingSort {
+    void coutingSort(int[] nums, int min, int max) {
+        int[] ctr = new int[max - min + 1];
+        for (int number : nums) {
+            ctr[number - min]++;
+        }
+        int z = 0;
+        for (int i = min; i <= max; i++) {
+            while (ctr[i - min] > 0) {
+                nums[z] = i;
+                z++;
+                ctr[i - min]--;
             }
         }
+    }
+
+    // Method to test above
+    public static main(String args[]) {
+        countingSort ob = new countingSort();
+        int nums[] = {7, -5, 3, 2, 1, 0, 45};
+        int minValue = -5, maxValue = 45;
+        System.out.println("Original Array:");
+        System.out.println(Arrays.toString(nums));
+        ob.coutingSort(nums, minValue, maxValue);
+        System.out.println("Sorted Array");
+        System.out.println(Arrays.toString(nums));
+    }
+}
 
 
-                        
