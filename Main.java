a558663ded74d2a6db1c4d5fc01f181feeb380d0
vassil-at <72628283+vassil-at@users.noreply.github.com>
2020-11-26 13:13:54 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,81 +1,36 @@
 import java.util.Arrays;
-
-public class PancakeSort {
-    int[] heap;
-
-    public String toString() {
-        String info = "";
-        for (int x : heap)
-            info += x + " ";
-        return info;
-    }
-
-    public void flip(int n) {
-        for (int i = 0; i < (n + 1) / 2; ++i) {
-            int tmp = heap[i];
-            heap[i] = heap[n - i];
-            heap[n - i] = tmp;
-        }
-        // System.out.printIn("flip(0.." + n + "): " + toString());
-    }
-
-    public int[] minmax(int n) {
-        int xm, xM;
-        xm = xM = heap[0];
-        int posm = 0, posM = 0;
-
-        for (int i = 1; i < n; ++i) {
-            if (heap[i] < xm) {
-                xm = heap[i];
-                posm = i;
-            } else if (heap[i] > xM) {
-                xM = heap[i];
-                posM = i;
+public class ShellSort {
+    public static void shell(int[] a) {
+        int increment = a.length / 2;
+        while (increment > 0) {
+            for (int i = increment; i < a.length; i++) {
+                int j = i;
+                int temp = a[i];
+                while (j >= increment && a[j - increment] > temp) {
+                    a[j] = a[j - increment];
+                    j = j - increment;
+                }
+                a[j] = temp;
+            }
+            if (increment == 2) {
+                increment = 1;
+            } else {
+                increment *= (5.0 / 11);
             }
         }
-        return new int[]{posm, posM};
     }
 
-    public void sort(int n, int dir) {
-        if (n == 0) return;
-
-        int[] mM = minmax(n);
-        int bestXPost = mM[dir];
-        int altXos = mM[1 - dir];
-        boolean flipped = false;
-
-        if (bestXPost == n - 1) {
-            --n;
-        } else if (bestXPost == 0) {
-            flip(n - 1);
-            --n;
-        } else if (altXos == n - 1) {
-            dir = 1 - dir;
-            --n;
-            flipped = true;
-        } else {
-            flip(bestXPost);
-        }
-        sort(n, dir);
-
-        if (flipped) {
-            flip(n);
-        }
-    }
-
-    PancakeSort(int[] numbers) {
-        heap = numbers;
-        sort(numbers.length, 1);
-    }
-
-    public static void main(String[] args) {
-        int numbers[] = {7, -5, 3, 2, 1, 0, 45};
+    // Method to test above
+    public static void main(String args[]) {
+        ShellSort ob = new ShellSort();
+        int nums[] = {7, -5, 3, 2, 1, 0, 45};
         System.out.println("Original Array:");
-        System.out.println(Arrays.toString(numbers));
-        PancakeSort pancakes = new PancakeSort(numbers);
+        System.out.println(Arrays.toString(nums));
+        ob.shell(nums);
         System.out.println("Sorted Array:");
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(nums));
     }
 }
-            
-        
+
+
+
