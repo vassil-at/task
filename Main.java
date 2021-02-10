7b4d83701069f6e908c42cfe7d2543f09976941a
vassil-at <72628283+vassil-at@users.noreply.github.com>
2021-02-10 12:25:51 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,8 @@
-import java.util.Arrays;
-public class ShellSort {
-    public static void shell(int[] a) {
-        int increment = a.length / 2;
-        while (increment > 0) {
-            for (int i = increment; i < a.length; i++) {
-                int j = i;
-                int temp = a[i];
-                while (j >= increment && a[j - increment] > temp) {
-                    a[j] = a[j - increment];
-                    j = j - increment;
-                }
-                a[j] = temp;
-            }
-            if (increment == 2) {
-                increment = 1;
-            } else {
-                increment *= (5.0 / 11);
-            }
+for (int index = 0 ; index < allGuesses.size() ; ++index) {
+        if (allGuesses.get(index) == input) {
+        System.out.println("You already used that character!");
+        return allGuesses;
         }
-    }
-
-    // Method to test above
-    public static void main(String args[]) {
-        ShellSort ob = new ShellSort();
-        int nums[] = {7, -5, 3, 2, 1, 0, 45};
-        System.out.println("Original Array:");
-        System.out.println(Arrays.toString(nums));
-        ob.shell(nums);
-        System.out.println("Sorted Array:");
-        System.out.println(Arrays.toString(nums));
-    }
-}
-
-
-
+        }
+        allGuesses.add(input);
+        return allGuesses;
