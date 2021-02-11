3058c3ae05a2794ae5a3e1b260e9a5cc42197e75
vassil-at <72628283+vassil-at@users.noreply.github.com>
2021-02-11 14:09:39 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,26 @@
-private void startGame(){
-        gameOver = false;
-        loop = new Timer(10, new ActionListener() {
-
-@Override
-public void actionPerformed(ActionEvent x) {
-
-        board.play();
-        frame.repaint();
-        }});
-        loop.start()}
-
-@Override
-public void paint(Graphics g) {
-
-        Point a = board.getBird().getLeftBottemCorner();
-        Point b = (board.getBird().getRightUpCorner());
-        g.setColor(Color.red);
-        g.fillRect(a.x, a.y, b.x - a.x, b.y - a.y);
-        drawHurdles(g);
-        g.setColor(Color.red);
-        g.drawString("Score: " + board.getBird().getHighScore(),10 , board.getBoardHight()+15);
-
-
+public static void main(String[] args) {
+        int arr[] = { 1, 9, 3, 10, 4, 20, 2};
+        ConsecutiveSequence(arr);
         }
-private void drawHurdles(Graphics g) {
-        g.setColor(Color.black);
-        ArrayList<Model.BOX> hurdles = board.getHindernisse().getHurdles();
-        Iterator<BOX> it = hurdles.iterator();
-        Point a;
-        Point b;
-        BOX akt;
-        int hoehe;
-        while (it.hasNext()) {
-        akt = it.next();
-        hoehe = akt.getRightUpCorner().y - akt.getLeftBottemCorner().y;
-        a = akt.getLeftBottemCorner();
-        b = akt.getRightUpCorner();
-        g.fillRect(a.x, a.y, b.x - a.x,  hoehe);
-        System.out.println(a + ", " + b + ", " + hoehe);
 
+public static void ConsecutiveSequence(int []a){
+        Arrays.sort(a);
+
+        int k =1;
+        int n = a.length;
+        int max_length=0;
+
+        for(int i =0;i<n-1;i++){
+        if(a[i]+1==a[i+1]){
+        k++;
+        if(i==n-2){
+        max_length=Math.max(max_length,k);
         }
+        }else{
+        max_length=Math.max(max_length,k);
+        k=1;
+        }
+        }
+
+        System.out.println(max_length);
         }
