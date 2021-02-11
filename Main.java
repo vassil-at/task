0c8810bf1e8d97d1f4d3836a2539ec9cf6a6f8d2
vassil-at <72628283+vassil-at@users.noreply.github.com>
2021-02-11 14:10:53 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,49 @@
-public class InputFilterMinMax implements InputFilter
-{
-    private final int min;
-    private final int max;
-    private final UtilityListener utilityListener;
+[INFO] ------------------------------------------------------------------------
+        [INFO] BUILD FAILURE
+        [INFO] ------------------------------------------------------------------------
+        [INFO] Total time: 3.215s
+        [INFO] Finished at: Thu Apr 16 09:32:46 CEST 2015
+        [INFO] Final Memory: 30M/72M
+        [INFO] ------------------------------------------------------------------------
+        [ERROR] Failed to execute goal org.apache.maven.plugins:maven-surefire-plugin:2.10:test (default-test) on project variant: There are test failures.
+        [ERROR]
+        [ERROR] Please refer to C:\Users\cschneid\Desktop\Projekt\workspaceCalc\Calculator\CalculatorModuleMain\target\surefire-reports for the individual test results.
+        [ERROR] -> [Help 1]
+        org.apache.maven.lifecycle.LifecycleExecutionException: Failed to execute goal org.apache.maven.plugins:maven-surefire-plugin:2.10:test (default-test) on project variant: There are test failures.
 
-    public InputFilterMinMax(int min, int max,UtilityListener utilityListener)
-    {
-        this.min = min;
-        this.max = max;
-        this.utilityListener = utilityListener;
-    }
+        Please refer to C:\Users\cschneid\Desktop\Projekt\workspaceCalc\Calculator\CalculatorModuleMain\target\surefire-reports for the individual test results.
+        at org.apache.maven.lifecycle.internal.MojoExecutor.execute(MojoExecutor.java:213)
+        at org.apache.maven.lifecycle.internal.MojoExecutor.execute(MojoExecutor.java:153)
+        at org.apache.maven.lifecycle.internal.MojoExecutor.execute(MojoExecutor.java:145)
+        at org.apache.maven.lifecycle.internal.LifecycleModuleBuilder.buildProject(LifecycleModuleBuilder.java:84)
+        at org.apache.maven.lifecycle.internal.LifecycleModuleBuilder.buildProject(LifecycleModuleBuilder.java:59)
+        at org.apache.maven.lifecycle.internal.LifecycleStarter.singleThreadedBuild(LifecycleStarter.java:183)
+        at org.apache.maven.lifecycle.internal.LifecycleStarter.execute(LifecycleStarter.java:161)
+        at org.apache.maven.DefaultMaven.doExecute(DefaultMaven.java:320)
+        at org.apache.maven.DefaultMaven.execute(DefaultMaven.java:156)
+        at org.apache.maven.cli.MavenCli.execute(MavenCli.java:537)
+        at org.apache.maven.cli.MavenCli.doMain(MavenCli.java:196)
+        at org.apache.maven.cli.MavenCli.main(MavenCli.java:141)
+        at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+        at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:57)
+        at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+        at java.lang.reflect.Method.invoke(Method.java:601)
+        at org.codehaus.plexus.classworlds.launcher.Launcher.launchEnhanced(Launcher.java:290)
+        at org.codehaus.plexus.classworlds.launcher.Launcher.launch(Launcher.java:230)
+        at org.codehaus.plexus.classworlds.launcher.Launcher.mainWithExitCode(Launcher.java:409)
+        at org.codehaus.plexus.classworlds.launcher.Launcher.main(Launcher.java:352)
+        Caused by: org.apache.maven.plugin.MojoFailureException: There are test failures.
 
-    @Override
-    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend)
-    {
-        try
-        {
-            int input = Integer.parseInt(dest.toString() + source.toString());
-
-            if (isInRange(min, max, input))
-            {
-                return null;
-            }
-            else
-            {
-                utilityListener.getItem("Value cannot exceed " + max);
-            }
-        }
-        catch (NumberFormatException ignored)
-        {
-            utilityListener.getItem("NumberFormatException");
-        }
-        return "";
-    }
-
-    private boolean isInRange(int a, int b, int c)
-    {
-        return b > a ? c >= a && c <= b : c >= b && c <= a;
-    }
+        Please refer to C:\Users\cschneid\Desktop\Projekt\workspaceCalc\Calculator\CalculatorModuleMain\target\surefire-reports for the individual test results.
+        at org.apache.maven.plugin.surefire.SurefireHelper.reportExecution(SurefireHelper.java:87)
+        at org.apache.maven.plugin.surefire.SurefirePlugin.writeSummary(SurefirePlugin.java:641)
+        at org.apache.maven.plugin.surefire.SurefirePlugin.handleSummary(SurefirePlugin.java:615)
+        at org.apache.maven.plugin.surefire.AbstractSurefireMojo.executeAfterPreconditionsChecked(AbstractSurefireMojo.java:137)
+        at org.apache.maven.plugin.surefire.AbstractSurefireMojo.execute(AbstractSurefireMojo.java:98)
+        at org.apache.maven.plugin.DefaultBuildPluginManager.executeMojo(DefaultBuildPluginManager.java:101)
+        at org.apache.maven.lifecycle.internal.MojoExecutor.execute(MojoExecutor.java:209)
+        ... 19 more
+        [ERROR]
+        [ERROR]
+        [ERROR] For more information about the errors and possible solutions, please read the following articles:
+        [ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
